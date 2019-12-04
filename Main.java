144cc02bfdfefc0651b78441b9dbedf0e2f0ca92
ryan17stehle <34487497+ryan17stehle@users.noreply.github.com>
2019-12-03 16:52:35 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ryan stehle
+ * this file just creates the loan application form.
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        InventorySearch search = new InventorySearch();
+        search.setVisible(true);
+    }
+    
+}
